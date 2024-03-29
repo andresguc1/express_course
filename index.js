4c4d6ba05823911a2c2154933b55bc6aade0bb51
Andresguc <andresguc@gmail.com>
2024-03-29 10:19:04 -0500

    feat(config): install express dependencies

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('my-app')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world from my Express Server');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('This is the endpoint /new-route');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'product1',
+    price: '100'
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server port: ' + port);
+});
